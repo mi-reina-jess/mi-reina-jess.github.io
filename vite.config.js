beadacaf390583b09ed597c9f11aa6b172965c06
farevalo <farevalo@comsatel.com.pe>
2025-02-05 11:12:40 -0500

    Se cambia configuracion

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-})
+  base: '/', // IMPORTANTE: Mantener '/' porque es un User Page
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+        manualChunks: undefined,  // 🔥 Esto evita fragmentación rara de archivos
+        format: 'es' // 🔥 Asegura que usa formato ES Modules
+      }
+    }
+  }
+});
